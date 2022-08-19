34689470abe72c40680b6ea0d4708cdf8b23f0ec
Blaccreed <62603834+Blaccreed@users.noreply.github.com>
2022-08-20 01:01:02 +0200

    Récupération des produits !

--- server/server.js
+++ server/server.js
@@ -1,8 +1,30 @@
+const { Pool, Client } = require('pg');
+const express = require('express');
+const app = express();
 
-const http = require('http');
-const app = require('./index');
+const pool = new Pool({
+  user: 'izjuxkkxxdkalz',
+  host: 'ec2-52-51-3-22.eu-west-1.compute.amazonaws.com',
+  database: 'd4l1m7v7u5orff',
+  password: '3b39bbccfb4537656523e47cba5857c24db0906074f2222c050a044a4caadb28',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+}
+})
 
-app.set('port', process.env.PORT || 3001);
-const server = http.createServer(app);
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-server.listen(process.env.PORT || 3001);
+app.get('/product', (req, res) => {
+    pool.query('select * from product', (err, result) =>{
+       console.log(err, res);
+       res.send(result.rows);
+       pool.end;
+    });
+});
+
+
+app.listen(5432, () => {
+    console.log("Sever is now listening at port");
+});
